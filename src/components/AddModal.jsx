a374152defbebb1ccf81e8fe49aeb58fc7a177bf
fatih <fsenyigit8@gmail.com>
2024-03-29 17:07:55 +0300

    modal settings

--- src/components/AddModal.jsx
+++ src/components/AddModal.jsx
@@ -1,43 +1,44 @@
 
+import { Container } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import Modal from "react-bootstrap/Modal";
 
-const AddModal = ({show,handleClose}) => {
-  
+const AddModal = ({show,handleClose, doctorName}) => {
   return (
     <>
       <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
+          <Modal.Title>{doctorName}</Modal.Title>
         </Modal.Header>
         <Modal.Body>
           <Form>
             <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-              <Form.Label>Email address</Form.Label>
+              <Form.Label>Patient Name:</Form.Label>
               <Form.Control
                 type="input"
-                placeholder="name@example.com"
+                placeholder="Enter full name"
                 autoFocus
+                required
               />
             </Form.Group>
             <Form.Group
               className="mb-3"
               controlId="exampleForm.ControlTextarea1"
             >
-              <Form.Label>Date</Form.Label>
+              <Form.Label>Date:</Form.Label>
               <Form.Control type="date" rows={3} />
             </Form.Group>
+            <Container className="d-flex gap-3 justify-content-center">
+              <Button type="submit" variant="success" onClick={handleClose}>
+                Save
+              </Button>
+              <Button variant="danger" onClick={handleClose}>
+                Close
+              </Button>
+            </Container>
           </Form>
         </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant="primary" onClick={handleClose}>
-            Save Changes
-          </Button>
-        </Modal.Footer>
       </Modal>
     </>
   );
--- src/components/Doctors.jsx
+++ src/components/Doctors.jsx
@@ -6,7 +6,12 @@ import { useState } from "react";
 
 const Doctors = () => {
     const [show, setShow] = useState(false)
-    const clickCard = () => {
+    const [doctorName, setDoctorName] = useState('')
+    const clickCard = (e) => {
+        const element = e.target
+        setDoctorName(
+          element.closest(".doctorCard").querySelector("h6").innerText,
+        );
        setShow(true)
     }
     const handleClose = () => setShow(false);
@@ -14,14 +19,22 @@ const Doctors = () => {
       <div className="doctors container">
         <div className="row gap-5 justify-content-center">
           {doctorData.map((doctor) => (
-            <div onClick={() => clickCard()} className="doctorCard">
+            <div key={doctor.id} onClick={(e) => clickCard(e)} className="doctorCard">
               <img src={doctor.img} alt="" />
               <h6>{doctor.name}</h6>
               <p>{doctor.dep}</p>
             </div>
           ))}
         </div>
-        <>{show && <AddModal show={show} handleClose={handleClose}></AddModal>}</>
+        <>
+          {show && (
+            <AddModal
+              doctorName={doctorName}
+              show={show}
+              handleClose={handleClose}
+            ></AddModal>
+          )}
+        </>
       </div>
     );
 }
